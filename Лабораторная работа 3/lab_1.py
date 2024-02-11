50239f3c46c8ebeb0815ead97db8131017584cb0
catchani <149243236+catchani@users.noreply.github.com>
2024-02-11 22:38:10 +0300

    Add files via upload
    Лабораторная работа №2.3

---
+++ Лабораторная работа 3/lab_1.py
@@ -0,0 +1,73 @@
+class Book:
+    """ Базовый класс книги. """
+
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        if not isinstance(pages, int):
+            raise TypeError("Кол-во страниц должно быть целым числом")
+        if pages <= 0:
+            raise ValueError("Кол-во страниц не может быть меньше 1")
+        self.pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Кол-во страниц должно быть целым числом")
+        self._pages = value
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        if not isinstance(duration, float):
+            raise TypeError("Продолжительность должна быть типа float")
+        if duration <= 0:
+            raise ValueError("Продолжительность должна быть больше 0")
+        self._duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value):
+        if not isinstance(value, float):
+            raise TypeError("Продолжительность должна быть типа float")
+        self._duration = value
+
+
+if __name__ == "__main__":
+    book1 = PaperBook("ITMO", "Katya", 10)
+    print(book1.pages)
+    book2 = AudioBook("Polytech", "Egor", 1.2)
+    print(book2.duration)
